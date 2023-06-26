a6059c3ca1ba562d11caa7bceaba108ccd1fc15e
rodriguezrrp <53585065+rodriguezrrp@users.noreply.github.com>
2023-06-26 11:01:57 -0400

    test forward slash in py script

--- prepare-docs-script.py
+++ prepare-docs-script.py
@@ -1,10 +1,10 @@
-with open('docs\index_copy.html', 'r') as f:  # 'r+' to support overwriting
+with open('./docs/index_copy.html', 'r') as f:  # gh action gives file not found; try forward slash
     contents = f.read()
 #    output = contents.format('testtext'='<br>Hello world, from python action!')
 #    f.seek(0)  # move pointer back to beginning
 #    f.write(output)
 #    f.truncate()  # remove anything existing after the place that was written to
 output = contents.format(testtext = '<br>Hello world, from python action!')
-with open('docs\index.html', 'w') as f:
+with open('./docs/index.html', 'w') as f:
     f.write(output)
     f.flush()
