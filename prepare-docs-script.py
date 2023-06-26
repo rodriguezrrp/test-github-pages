21f3b8ad11dfc7654b95247aee705b9f719f7da3
rodriguezrrp <53585065+rodriguezrrp@users.noreply.github.com>
2023-06-26 10:40:04 -0400

    testing custom action to execute python file before GH Pages publishes

---
+++ prepare-docs-script.py
@@ -0,0 +1,10 @@
+with open('docs\index_copy.html', 'r') as f:  # 'r+' to support overwriting
+    contents = f.read()
+#    output = contents.format('testtext'='<br>Hello world, from python action!')
+#    f.seek(0)  # move pointer back to beginning
+#    f.write(output)
+#    f.truncate()  # remove anything existing after the place that was written to
+output = contents.format('testtext'='<br>Hello world, from python action!')
+with open('docs\index.html', 'w') as f:
+    f.write(output)
+    f.flush()
